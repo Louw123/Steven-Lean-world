335eb566207eb3fcfdbf0638d38c365094c068a8
Louw123 <95922931+Louw123@users.noreply.github.com>
2024-10-26 22:31:33 +0000

    beginning

---
+++ Game/Levels/Inception.lean
@@ -0,0 +1,14 @@
+import GameServer.Commands
+
+World "Inception"
+Title "Inception"
+
+Introduction
+"
+Garnet: In order to begin, I have to show you how lean prove things.
+There are two parts that are import for you, and so I have to unfuse myself in order to explain.
+There are two areas you can go. Both of them build on eachother.
+So completing one will help you figure out how to complete the other.
+
+Ruby and saphire: So lets begin.
+"
---
+++ Game/Levels/Ruby/Simple_tactics.lean
@@ -0,0 +1,26 @@
+import GameServer.Commands
+World "Ruby"
+Level 1
+Title "Hello World"
+
+Introduction "
+Lets look at our first tactic: `rfl`
+
+rfl stands for `reflexivity of equality`. This states that a=a will be allways be a=a no matter what you change.
+Try it out.
+"
+
+/-- The exercise statement in natural language using latex: $\iff$. -/
+Statement: n = n := by
+  rfl
+
+Conclusion "
+There you go.
+"
+/--
+rfl stands for `reflexivity of equality`. This states that a=a will allways be a=a no matter what you change.
+This is useful in closing arrithmatic goals. 
+-/
+TacticDoc rfl
+
+NewTactic rfl
